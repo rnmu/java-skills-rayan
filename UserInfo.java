fdd0ce6a4205246d11e3ee9a49fa2d38f8516d5a
rayan <rayanalhashmi8@gmail.com>
2025-07-22 10:31:41 +0400

    Added UserInfo class with displayinfo method

---
+++ UserInfo.java
@@ -0,0 +1,49 @@
+public class UserInfo {
+    private String name;
+    private int age;
+    private String email;
+    private boolean isActive;
+
+    // Constructor
+    public UserInfo(String name, int age , String email, boolean isActive) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.isActive= isActive;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public void setIsActive(boolean isActive){
+        this.isActive = isActive;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public boolean getIsActive() {
+        return isActive;
+    }
+
+    public void displayInfo(){
+        System.out.println("User details");
+        System.out.println("Name:   " + name);
+        System.out.println("Age:    " + age);
+        System.out.println("Email:   " + email);
+        System.out.println("isActive:  " + isActive);
+    }
+
+
+}
--- javaskills.java
+++ javaskills.java
@@ -1,6 +1,27 @@
 public class javaskills {
 
     public static void main( String [] args){
-        System.out.println("test");
+
+        // 3 different variables
+
+        UserInfo user1 = new UserInfo("Rayan",22,"abc@gmail.com",true);
+        UserInfo user2 = new UserInfo("Noor",20,"mb3@gmail.com",false);
+        UserInfo user3 = new UserInfo("Ahmed",15,"ah23@gmail.com",true);
+
+//      //Displays user information
+        System.out.println("Displays User information:  ");
+        System.out.println("User 1 : ");
+        user1.displayInfo();
+        System.out.println("User 2 : ");
+        user2.displayInfo();
+        System.out.println("User 3 : ");
+        user3.displayInfo();
+
+        //Test get and set
+        System.out.println(user1.getAge());
+        
+
+
+
     }
 }
